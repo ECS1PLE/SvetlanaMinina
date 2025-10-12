661f68c76bb32c37c6adf4b057bca6d18fcbf374
ECS1PLE <119251348+ECS1PLE@users.noreply.github.com>
2025-10-12 14:33:53 +0300

    Fix Header And Photos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,39 @@ export const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="logo">
-        <div>
-          <span style={{ marginTop: "auto" }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-target w-6 h-6 text-white"
-              aria-hidden="true"
-              style={{ marginTop: "auto" }}
-            >
-              <circle cx="12" cy="12" r="10"></circle>
-              <circle cx="12" cy="12" r="6"></circle>
-              <circle cx="12" cy="12" r="2"></circle>
-            </svg>
-          </span>
+        <div
+          style={{
+            backgroundColor: "#e66d00",
+            width: "30px",
+            height: "30px",
+            borderRadius: "3px",
+            display: "flex",
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-target w-6 h-6 text-white"
+            aria-hidden="true"
+            style={{
+              marginTop: "auto",
+              marginBottom: "auto",
+              marginLeft: "auto",
+              marginRight: "auto",
+              color: "white",
+            }}
+          >
+            <circle cx="12" cy="12" r="10"></circle>
+            <circle cx="12" cy="12" r="6"></circle>
+            <circle cx="12" cy="12" r="2"></circle>
+          </svg>
         </div>
         <span>Светлана Минина</span>
       </div>
